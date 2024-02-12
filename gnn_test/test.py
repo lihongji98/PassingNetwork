02610fa9e1527b54ceeebff0bbe4f2288847f60e
lihong.ji <294672570@qq.com>
2024-02-12 14:34:15 +0100

    gnn code test

---
+++ gnn_test/test.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.datasets import Planetoid
+
+dataset = Planetoid(root='/tmp/Cora', name='Cora')
+print(dataset[0].num_nodes, dataset[0].num_node_features)
+print("data edge:", dataset[0].edge_index.shape)
+print("data node_attr", dataset[0].x.shape)
+
+
+class GCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # here to define the network channel
+        self.conv1 = GCNConv(dataset.num_node_features, 16)
+        self.conv2 = GCNConv(16, dataset.num_classes)
+
+    def forward(self, _data):
+        x, edge_index = _data.x, _data.edge_index
+
+        # here to input the attributes of nodes and edges
+        x = self.conv1(x, edge_index)
+        print(x.shape)
+        x = F.gelu(x)
+        x = F.dropout(x, training=self.training)
+        x = self.conv2(x, edge_index)
+
+        return F.log_softmax(x, dim=1)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = GCN().to(device)
+data = dataset[0].to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
+
+model.train()
+for epoch in range(200):
+    optimizer.zero_grad()
+    out = model(data)
+    loss = F.cross_entropy(out[data.train_mask], data.y[data.train_mask])
+    loss.backward()
+    optimizer.step()
+
+model.eval()
+pred = model(data).argmax(dim=1)
+correct = (pred[data.test_mask] == data.y[data.test_mask]).sum()
+acc = int(correct) / int(data.test_mask.sum())
+print(f'Accuracy: {acc:.4f}')
