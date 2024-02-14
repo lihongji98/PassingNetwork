1dd2d91273f5fbcc4472d77f4866e122cc6eed91
Joe <122629088+Joe-McMullen@users.noreply.github.com>
2024-02-14 20:03:20 +0100

    network_demo

--- db_connect_utils.py
+++ db_connect_utils.py
@@ -25,12 +25,18 @@ def db_disconnect():
 
 def db_connect_pymongo():
 
-    file_path = "./mongodb_url.txt"
-    with open(file_path, 'r') as file:
-        host_address = file.read().strip()
+    try:
+        file_path = "/Users/lihongji/mongodb_url.txt"
+        with open(file_path, 'r') as file:
+            host_address = file.read().strip()
+    except FileNotFoundError:   
+        file_path = r"C:\Users\joemc\Documents\UPC_local\PassingNetwork\mongodb_url.txt"
+        with open(file_path, 'r') as file:
+            host_address = file.read().strip()
 
     try:
-        return pymongo.MongoClient(host_address)
+        client = pymongo.MongoClient(host_address)
+        return client['LaLiga2023']
         print("connected to mongodb database successfully!")
     except RuntimeError:
         print("connection failed!")
